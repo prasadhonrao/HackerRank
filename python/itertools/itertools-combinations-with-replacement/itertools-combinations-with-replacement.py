e81d2370dc2fac58e28d97099a6a07f5a1212d04
Prasad Honrao <Honrao.Prasad@Gmail.com>
2019-03-23 08:05:45 +0530

    Add itertools-combinations-with-replacement problem stmt, solution, update README

---
+++ python/itertools/itertools-combinations-with-replacement/itertools-combinations-with-replacement.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from itertools import combinations_with_replacement
+
+s, k = input().split()
+
+for c in combinations_with_replacement(sorted(s), int(k)):
+    print("".join(c))
