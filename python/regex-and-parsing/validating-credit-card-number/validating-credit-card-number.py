f56548c22acb83a378b3a7ea70b173bef5358686
Prasad Honrao <Honrao.Prasad@Gmail.com>
2019-04-12 03:58:37 +0000

    Add validating credit card number problem stmt, solution, update README

---
+++ python/regex-and-parsing/validating-credit-card-number/validating-credit-card-number.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import re
+TESTER = re.compile(
+    r"^"
+    r"(?!.*(\d)(-?\1){3})"
+    r"[456]"
+    r"\d{3}"
+    r"(?:-?\d{4}){3}"
+    r"$")
+for _ in range(int(input().strip())):
+    print("Valid" if TESTER.search(input().strip()) else "Invalid")
