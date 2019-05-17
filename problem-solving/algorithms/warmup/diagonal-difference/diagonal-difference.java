1b0b965e7dfc3572ce45c7b9ca69cf8930753f83
Prasad Honrao <Honrao.Prasad@Gmail.com>
2019-05-17 03:22:59 +0000

    Add diagonal difference java solution

---
+++ problem-solving/algorithms/warmup/diagonal-difference/diagonal-difference.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+        int primary = 0;
+        int secondary = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n ; i++)
+        {
+            primary += arr[i][i];
+        }
+
+        for (int j = 0; j < arr.length ; j++)
+        {
+            secondary += arr[j][n - 1];
+            n--;
+        }
+
+        return Math.abs(primary - secondary);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
