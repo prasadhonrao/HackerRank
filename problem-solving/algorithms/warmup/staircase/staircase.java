54bdcd480b32055472f195c1b4b540d9577c20e3
Prasad Honrao <Honrao.Prasad@Gmail.com>
2019-05-20 00:39:01 +0000

    Add staircase java solution, update README

---
+++ problem-solving/algorithms/warmup/staircase/staircase.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for (int count = 1; count <= n ; count++) {
+            for (int i = n - count; i > 0 ; i--) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 0; j < count; j++) {
+                System.out.print("#");    
+            }
+            System.out.println();
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
