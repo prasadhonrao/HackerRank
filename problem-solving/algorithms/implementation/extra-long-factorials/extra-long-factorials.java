920186649df1184808e2484af4b8638f23064d53
Prasad Honrao <Honrao.Prasad@Gmail.com>
2019-06-21 16:45:43 +0000

    Add extra long factorials java solution

---
+++ problem-solving/algorithms/implementation/extra-long-factorials/extra-long-factorials.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+        BigInteger factorial = new BigInteger("1");
+        while (n > 1) {
+            factorial = factorial.multiply(BigInteger.valueOf(n));
+            n--;
+        }
+        System.out.println(factorial);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
